ad7f3f9cc19e7468031cda3597728122c444b837
Jmolina20 <julianmolina2011@gmail.com>
2022-02-25 20:34:55 -0500

    Hello World in Netbeans

---
+++ netbeans/HelloWorld/src/main/java/com/mthree/Hello.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mthree;
+
+/**
+ *
+ * @author Julian Molina
+ * email: julianmolina2011@gmail.com
+ * date: 02/25/2022
+ * purpose: My first hello world program in an IDE
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Elizabeth!");
+    }
+}
